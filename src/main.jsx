8b500c945903ba96033369faf8fa0b0cd23ff8ce
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-10-30 16:00:04 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render("hej");
+const navbar = (
+  <nav>
+    <h1>Bob Bistro</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+createRoot(document.getElementById("root")).render(navbar);
