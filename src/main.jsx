c6b851c282e96b45ee01d7ee27035e44ccac331f
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-10-30 16:44:44 +0100

    Custom Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,31 @@
 import { createRoot } from "react-dom/client";
 
-const page = (
-  <>
-    <img src="./src/assets/react-logo.png" alt="react-logo" width="40px" />
-    <h1>Fun facts about React</h1>
-    <ul>
+// const page = (
+//   <>
+//     <img src="./src/assets/react-logo.png" alt="react-logo" width="40px" />
+//     <h1>Fun facts about React</h1>
+//     <ul>
+//       <li>Was first released in 2013</li>
+//       <li>Was originally created by Jordan Walke</li>
+//       <li>Has well over 100K stars on Github</li>
+//       <li>Is maintained by Facebook</li>
+//       <li>Powers thousands of enterprise apps, including mobile apps</li>
+//     </ul>
+//   </>
+// );
+
+// createRoot(document.getElementById("root")).render(page);
+
+function Reasons() {
+  return (
+    <ol>
       <li>Was first released in 2013</li>
       <li>Was originally created by Jordan Walke</li>
       <li>Has well over 100K stars on Github</li>
       <li>Is maintained by Facebook</li>
       <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </>
-);
+    </ol>
+  );
+}
 
-createRoot(document.getElementById("root")).render(page);
+createRoot(document.getElementById("root")).render(<Reasons />);
