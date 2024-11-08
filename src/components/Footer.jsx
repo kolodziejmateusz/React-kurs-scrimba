1524d890cfc62d231967e262b73f246b352574a7
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-11-08 15:03:42 +0100

    Organize components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer>
+    <small>© 2024 Mateusz development. All rights reserved.</small>
+  </footer>
+);
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./main.css";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/MainContent.jsx";
+import Footer from "./components/Footer.jsx";
 
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img
-          className="nav-logo"
-          src="./src/assets/react-logo.png"
-          alt="react-logo"
-        />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
-    </>
-  );
-}
-
-const Footer = () => (
-  <footer>
-    <small>© 2024 Mateusz development. All rights reserved.</small>
-  </footer>
-);
-
-function Page() {
+function App() {
   return (
     <>
       <Header />
@@ -50,8 +14,4 @@ function Page() {
   );
 }
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <Page />
-  </>
-);
+createRoot(document.getElementById("root")).render(<App />);
