f09f2d5886904ef14f4fcc25e7d5dc74927f04ef
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-11-08 14:25:16 +0100

    Parent/Child Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,52 @@
 import { createRoot } from "react-dom/client";
 
-// const page = (
-//   <>
-//     <img src="./src/assets/react-logo.png" alt="react-logo" width="40px" />
-//     <h1>Fun facts about React</h1>
-//     <ul>
-//       <li>Was first released in 2013</li>
-//       <li>Was originally created by Jordan Walke</li>
-//       <li>Has well over 100K stars on Github</li>
-//       <li>Is maintained by Facebook</li>
-//       <li>Powers thousands of enterprise apps, including mobile apps</li>
-//     </ul>
-//   </>
-// );
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./src/assets/react-logo.png" alt="react-logo" width="40px" />
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-// createRoot(document.getElementById("root")).render(page);
+function MainContent() {
+  return (
+    <>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </>
+  );
+}
+
+const Footer = () => (
+  <footer>
+    <small>© 2024 Mateusz development. All rights reserved.</small>
+  </footer>
+);
 
 function Page() {
   return (
     <>
-      <header>
-        <nav>
-          <img
-            src="./src/assets/react-logo.png"
-            alt="react-logo"
-            width="40px"
-          />
-        </nav>
-      </header>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          Its a popular library, so Ill be able to fit in with the cool kids!
-        </li>
-        <li>Im more likely to get a job as a developer if I know React</li>
-      </ol>
-      <footer>
-        <small>© 2024 Mateusz development. All rights reserved.</small>
-      </footer>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Page />
-    <Page />
     <Page />
   </>
 );
