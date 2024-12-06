6760f27d93ea33a5f62daf42ad8b1419bb414c80
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-12-06 15:14:06 +0100

    Props part 3: Create a contact component

---
+++ Section2-Data-Driven-React/src/App2.jsx
@@ -0,0 +1,15 @@
+import Contact from "./components/Contact";
+import "./app2.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="contacts">
+        <Contact />
+        <Contact />
+        <Contact />
+        <Contact />
+      </div>
+    </>
+  );
+}
