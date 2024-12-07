410fc1f1fd20d394d232bbaa5ce5b615b57ed0ed
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-12-07 17:15:39 +0100

    Contact cards

--- Section2-Data-Driven-React/eslint.config.js
+++ Section2-Data-Driven-React/eslint.config.js
@@ -28,8 +28,12 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      "react/prop-types": 0,
       "react/jsx-no-target-blank": "off",
-      "react/no-unescaped-entities": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   },
 ];
--- Section2-Data-Driven-React/src/App2.jsx
+++ Section2-Data-Driven-React/src/App2.jsx
@@ -5,10 +5,30 @@ export default function App() {
   return (
     <>
       <div className="contacts">
-        <Contact />
-        <Contact />
-        <Contact />
-        <Contact />
+        <Contact
+          img="./src/images/mr-whiskerson.png"
+          name="Mr. Whiskerson"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+        />
+        <Contact
+          img="./src//images/fluffykins.png"
+          name="Fluffykins"
+          phone="(212) 555-2345"
+          email="fluff@me.com"
+        />
+        <Contact
+          img="./src//images/felix.png"
+          name="Felix"
+          phone="(212) 555-4567"
+          email="thecat@hotmail.com"
+        />
+        <Contact
+          img="./src//images/pumpkin.png"
+          name="Pumpkin"
+          phone="(0800) CAT KING"
+          email="pumpkin@scrimba.com"
+        />
       </div>
     </>
   );
