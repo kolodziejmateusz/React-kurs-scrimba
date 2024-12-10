013709c44ac4e52bd6b80f649474eb90374bb315
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-12-10 15:48:36 +0100

    Chef Claude: Map ingredients list

--- Section3-React-State-Chef-Claude/src/components/Main.jsx
+++ Section3-React-State-Chef-Claude/src/components/Main.jsx
@@ -1,14 +1,32 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted!");
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    console.log(newIngredient);
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
     </main>
   );
 }
