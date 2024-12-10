870b1f47b6b232a64a165a71a411160856cd401c
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-12-10 12:41:57 +0100

    Chef Claude: Header

---
+++ Section3-React-State-Chef-Claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <img src="/src/images/chef-claude-icon.png" alt="chef-claude-icon" />
+        <h1>Chef Claude</h1>
+      </header>
+    </>
+  );
+}
