703a9be607718b41c6dc3001076b74e846f7c1ed
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-01-02 18:34:44 +0100

    Section2-Data-Driven-React - Added new places at data file

--- Section2-Data-Driven-React/src/data.js
+++ Section2-Data-Driven-React/src/data.js
@@ -35,4 +35,40 @@ export default [
     dates: "01 Oct, 2021 - 18 Nov, 2021",
     text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
   },
+  {
+    id: 4,
+    img: {
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/240px-Colosseo_2020.jpg",
+      alt: "Colosseum",
+    },
+    title: "Colosseum",
+    country: "Italy",
+    googleMapsLink: "https://maps.app.goo.gl/Bamu4VgEQ8xjhgRD6",
+    dates: "15 Mar, 2022 - 29 Mar, 2022",
+    text: "The Colosseum is an ancient amphitheatre in the centre of Rome, Italy. It is one of the largest amphitheatres ever built and is considered one of the greatest works of Roman architecture and engineering.",
+  },
+  {
+    id: 5,
+    img: {
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Pyramids_of_the_Giza_Necropolis.jpg/800px-Pyramids_of_the_Giza_Necropolis.jpg",
+      alt: "Pyramids of Giza",
+    },
+    title: "Pyramids of Giza",
+    country: "Egypt",
+    googleMapsLink: "https://maps.app.goo.gl/yTwvAc8tYjxVP3N8A",
+    dates: "03 Aug, 2022 - 17 Aug, 2022",
+    text: "The Pyramids of Giza are ancient pyramid structures in Egypt, known for being one of the Seven Wonders of the Ancient World. These monumental structures have been standing for thousands of years.",
+  },
+  {
+    id: 6,
+    img: {
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Christ_the_Redeemer_-_Cristo_Redentor.jpg/800px-Christ_the_Redeemer_-_Cristo_Redentor.jpg",
+      alt: "Christ the Redeemer",
+    },
+    title: "Christ the Redeemer",
+    country: "Brazil",
+    googleMapsLink: "https://maps.app.goo.gl/pekLoJnUrzE4iEzm7",
+    dates: "12 Dec, 2022 - 22 Dec, 2022",
+    text: "Christ the Redeemer is a statue of Jesus Christ in Rio de Janeiro, Brazil. It stands 30 meters tall and is one of the most iconic landmarks in South America.",
+  },
 ];
