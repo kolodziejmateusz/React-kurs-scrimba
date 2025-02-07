b842272a7177e0468d6eb1d771f3829d69133e8a
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-07 15:35:46 +0100

    Section4-Side-Effects-Meme-Generator-app init

---
+++ Section4-Side-Effects-Meme-Generator-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./app.css";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
