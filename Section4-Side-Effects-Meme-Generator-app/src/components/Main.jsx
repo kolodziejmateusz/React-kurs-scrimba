28a21fb509fb5c7f00e5bdd58da27d9083a8fb0f
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-15 17:49:48 +0100

    Section4 - Meme Generator app - completed

--- Section4-Side-Effects-Meme-Generator-app/src/components/Main.jsx
+++ Section4-Side-Effects-Meme-Generator-app/src/components/Main.jsx
@@ -15,27 +15,20 @@ export default function Main() {
       .then((data) => setMemesArr(data.data.memes));
   }, []);
 
-  /**
-   * Challenge:
-   * Get an array of memes from the imgflip API as soon as
-   * this component renders for the first time.
-   * Check the imgflip documentation for the correct URL.
-   * Save the array of memes (not the whole response
-   * data) to state. (For this app, we'll randomly choose
-   * one of the memes from this array when the user clicks
-   * the "Get a new meme image" button, but we'll do that in
-   * a separate challenge.)
-   *
-   * Hint: for now, don't try to use an async/await function.
-   * Instead, use `.then()` to resolve the promises
-   * from using `fetch`. We'll learn why after this challenge.
-   */
-
   function handleChange(event) {
     const { value, name } = event.currentTarget;
     setMeme((prevMeme) => ({
       ...prevMeme,
       [name]: value,
+    }));
+  }
+
+  function handleGetNewMeme() {
+    const randomNumber = Math.floor(Math.random() * memesArr.length);
+    console.log(randomNumber);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: memesArr[randomNumber].url,
     }));
   }
 
@@ -63,7 +56,7 @@ export default function Main() {
             value={meme.bottomText}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={handleGetNewMeme}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={meme.imageUrl} />
