e33a905f6c27af2b9918e5bcde9a9c26ab668643
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-17 14:54:47 +0100

    Sneak peak: refs

--- Section3-React-State-Chef-Claude/src/components/Main.jsx
+++ Section3-React-State-Chef-Claude/src/components/Main.jsx
@@ -1,26 +1,15 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import IngredientsList from "./ingredientsList";
 import { getRecipeFromMistral } from "../ai";
 
 export default function Main() {
-  /**
-   * Wyzwanie: Uzyskaj przepis od AI!
-   *
-   * To będzie trochę trudniejsze wyzwanie, które wymaga od Ciebie
-   * krytycznego myślenia i zastosowania umiejętności, które
-   * do tej pory ćwiczyłeś(-aś) i poznawałeś(-aś).
-   *
-   * Używając funkcji `getRecipeFromChefClaude` lub
-   * `getRecipeFromMistral`, spraw, aby po kliknięciu przez użytkownika
-   * przycisku "Uzyskaj przepis", tekstowa odpowiedź od AI była wyświetlana
-   * w komponencie <ClaudeRecipe>.
-   *
-   * Na razie renderuj surowy markdown, który zwraca AI,
-   * nie martw się jeszcze o to, żeby wyglądało to ładnie.
-   * (Wkrótce użyjemy pakietu, który zajmie się renderowaniem markdown za nas.)
-   */
-  const [ingredients, setIngredients] = useState([]);
+  const [ingredients, setIngredients] = useState([
+    "makaron",
+    "zioła",
+    "kurczak",
+    "papryka",
+  ]);
 
   function addIngredients(formData) {
     const newIngredient = formData.get("ingredient");
@@ -29,6 +18,8 @@ export default function Main() {
   }
 
   const [recipe, setRecipe] = useState(null);
+  const recipeSection = useRef(null);
+  console.log(recipeSection);
 
   async function getRecipe() {
     const generatedRecipe = await getRecipeFromMistral(ingredients);
@@ -49,7 +40,11 @@ export default function Main() {
       </form>
 
       {ingredients.length > 0 && (
-        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+        <IngredientsList
+          ingredients={ingredients}
+          getRecipe={getRecipe}
+          ref={recipeSection}
+        />
       )}
 
       {recipe && <ClaudeRecipe recipe={recipe} />}
