1d603437d93a4b9c8ed70c6ef444712c5448693b
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-25 11:21:39 +0100

    Assembly Endgame - Header Section

--- Assembly-Endgame-Capstone-Project/src/App.jsx
+++ Assembly-Endgame-Capstone-Project/src/App.jsx
@@ -1,3 +1,20 @@
+import "./app.css";
+/**
+ * Goal: Build out the main parts of our app
+ *
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
+ */
 export default function App() {
-  return <>hej</>;
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+    </main>
+  );
 }
