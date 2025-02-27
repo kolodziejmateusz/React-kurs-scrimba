2de37622e561d147f6e07a4e1e69b84e45e602f4
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-27 20:05:19 +0100

    Assembly Endgame - Display missed letters when lost

--- Assembly-Endgame-Capstone-Project/src/App.jsx
+++ Assembly-Endgame-Capstone-Project/src/App.jsx
@@ -81,13 +81,25 @@ export default function App() {
     );
   });
 
-  const letterElements = currentWord
-    .split("")
-    .map((letter, index) => (
-      <span key={index}>
-        {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+  // const letterElements = currentWord
+  //   .split("")
+  //   .map((letter, index) => (
+  //     <span key={index}>
+  //       {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+  //     </span>
+  //   ));
+
+  const letterElements = currentWord.split("").map((letter, index) => {
+    const shouldRevealLetter = isGameLost || guessedLetters.includes(letter);
+    const letterClassName = clsx(
+      isGameLost && !guessedLetters.includes(letter) && "missed-letter"
+    );
+    return (
+      <span key={index} className={letterClassName}>
+        {shouldRevealLetter ? letter.toUpperCase() : ""}
       </span>
-    ));
+    );
+  });
 
   const gameStatusClass = clsx("game-status", {
     won: isGameWon,
