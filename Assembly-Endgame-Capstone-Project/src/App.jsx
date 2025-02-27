6736749664e29ed994a8cbb38f512dff8487c09c
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-27 14:46:16 +0100

    Assembly Endgame - Display won/lost status

--- Assembly-Endgame-Capstone-Project/src/App.jsx
+++ Assembly-Endgame-Capstone-Project/src/App.jsx
@@ -9,12 +9,13 @@ export default function App() {
    * Goal: Add in the incorrect guesses mechanism to the game
    *
    * Challenge:
-   * 1. Create a variable `isGameOver` which evaluates to `true`
-   *    if the user has guessed incorrectly 8 times. Consider how
-   *    we might make this more dynamic if we were ever to add or
-   *    remove languages from the languages array.
-   * 2. Conditionally render the New Game button only if the game
-   *    is over.
+   * Conditionally render either the "won" or "lost" statuses
+   * from the design, both the text and the styles, based on the
+   * new derived variables.
+   *
+   * Note: We always want the surrounding `section` to be rendered,
+   * so only change the content inside that section. Otherwise the
+   * content on the page would jump around a bit too much.
    */
 
   // State values
@@ -70,6 +71,11 @@ export default function App() {
       </span>
     ));
 
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+  });
+
   return (
     <main>
       <header>
@@ -79,9 +85,20 @@ export default function App() {
           from Assembly!
         </p>
       </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done!🎉</p>
+      <section className={gameStatusClass}>
+        {isGameOver ? (
+          isGameWon ? (
+            <>
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+            </>
+          ) : (
+            <>
+              <h2>Game over!</h2>
+              <p>You lose! Better start learning Assembly 😭</p>
+            </>
+          )
+        ) : null}
       </section>
       <section className="language-chips">
         {languageArr.map((language, index) => (
