6167b0d660a9392f3f2ae83a47c63a6511896353
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-02-27 19:20:42 +0100

    Assembly Endgame - Make the game more a11y-friendly part 2

--- Assembly-Endgame-Capstone-Project/src/App.jsx
+++ Assembly-Endgame-Capstone-Project/src/App.jsx
@@ -23,15 +23,14 @@ export default function App() {
   const [guessedLetters, setGuessedLetters] = useState([]);
 
   // Derived values
+  const numGuessesLeft = languageArr.length - 1;
   const wrongGuessCount = guessedLetters.filter(
     (letter) => !currentWord.includes(letter)
   ).length;
-  // console.log(wrongGuessCount);
-
   const isGameWon = currentWord
     .split("")
     .every((letter) => guessedLetters.includes(letter));
-  const isGameLost = wrongGuessCount >= languageArr.length - 1;
+  const isGameLost = wrongGuessCount >= numGuessesLeft;
   const isGameOver = isGameWon || isGameLost;
   const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
   const isLastGuessIncorrect =
@@ -139,7 +138,15 @@ export default function App() {
         ))}
       </section>
       <section className="word">{letterElements}</section>
+
+      {/* Combined visually-hidden aria-live region for status updates */}
       <section className="sr-only" aria-live="polite" role="status">
+        <p>
+          {currentWord.includes(lastGuessedLetter)
+            ? `Correct! The letter ${lastGuessedLetter} is in the word.`
+            : `Sorry, the letter ${lastGuessedLetter} is not in the word.`}
+          You have {numGuessesLeft} attempts left.
+        </p>
         <p>
           Current word:{" "}
           {currentWord
