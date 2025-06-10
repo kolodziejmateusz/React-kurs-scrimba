bbc3350645f4e4d965903d846feb2a07292540b3
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-06-10 16:15:32 +0200

    Boilerplate code - zadanie 1

--- Memory-Game-Project/src/App.jsx
+++ Memory-Game-Project/src/App.jsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import MemoryCard from "./components/MemoryCard";
+import "./App.css";
+
 export default function App() {
-  return <>hej</>;
+  const [isGameOn, setIsGameOn] = useState(false);
+
+  function startGame(e) {
+    e.preventDefault();
+    setIsGameOn(true);
+  }
+
+  function turnCard() {
+    console.log("Memory card clicked");
+  }
+
+  return (
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} />}
+    </main>
+  );
 }
