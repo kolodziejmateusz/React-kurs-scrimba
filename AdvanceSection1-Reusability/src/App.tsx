fd1d26cdfb5942205110bdd16413fae01d80226f
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-08-13 17:22:59 +0200

    Button - size prop

--- AdvanceSection1-Reusability/src/App.tsx
+++ AdvanceSection1-Reusability/src/App.tsx
@@ -3,14 +3,14 @@ import Button from "./components/Button";
 import { FcGoogle } from "react-icons/fc";
 
 function App() {
-  function onClick(): void {
-    console.log("Logging in...");
-  }
+  // function onClick(): void {
+  //   console.log("Logging in...");
+  // }
 
   return (
     <>
       <main>
-        <Button variant="red" style={{ color: "green" }} onClick={onClick}>
+        <Button size="lg" className="green">
           <FcGoogle />
           Log in with Google
         </Button>
--- AdvanceSection1-Reusability/src/components/Button.tsx
+++ AdvanceSection1-Reusability/src/components/Button.tsx
@@ -1,9 +1,29 @@
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
-  variant: "red" | "blue" | "green";
+  // variant: "red" | "blue" | "green";
+  size: "sm" | "lg";
 };
 
-export default function Button({ children, variant, ...rest }: ButtonProps) {
-  console.log(variant);
-  return <button {...rest}>{children}</button>;
+export default function Button({ children, size, ...rest }: ButtonProps) {
+  /**
+   * Challenge: accept the `size` prop and set the `className` of the
+   * <button> to:
+   *
+   * "button-small" if `size` is "sm"
+   * "button-large" if `size` is "lg"
+   *
+   * Note: don't try to manually add a `className` to the Button
+   * instance in index.js yet. (DO add `className` to this file
+   * as part of the challenge, though.)
+   */
+
+  let sizeClass: "button-small" | "button-large" | undefined;
+  if (size === "sm") sizeClass = "button-small";
+  if (size === "lg") sizeClass = "button-large";
+
+  return (
+    <button className={sizeClass} {...rest}>
+      {children}
+    </button>
+  );
 }
