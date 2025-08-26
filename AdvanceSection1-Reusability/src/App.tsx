9fa2346f36198a633ef1f3bb492bc86462e32a32
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-08-26 15:22:58 +0200

    createContext() & Context Provider

--- AdvanceSection1-Reusability/src/App.tsx
+++ AdvanceSection1-Reusability/src/App.tsx
@@ -1,15 +1,20 @@
 import "./App.css";
-// import React from "react"
+import React from "react";
 import Header from "./components/Header";
 import Button from "./components/Button";
 
+const ThemeContext = React.createContext("");
+
 function App() {
   return (
-    <div className="container dark-theme">
-      <Header />
-      <Button />
-    </div>
+    <ThemeContext.Provider value="light">
+      <div className="container dark-theme">
+        <Header />
+        <Button />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
 export default App;
+export { ThemeContext };
